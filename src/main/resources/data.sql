dbc4eb35b1f44d1d20ee57f0c3447dc54f60a9a5
rui27sui <125226054+rui27sui@users.noreply.github.com>
2023-08-08 23:06:54 +0900

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 insert into Product(product_id, name, price, explanation, stock, seller)
-values (11, 'LoccitaneHandcream', 30000, '핸드크림', 550, 'Li-pot');
+values (11, 'LoccitaneHandcream', 30000, '핸드크림', 550, 'seller');
 
 insert into Product(product_id, name, price, explanation, stock, seller)
-values (12, 'LoccitaneHandwash', 28000, '핸드워시', 100, 'Li-pot');
+values (12, 'LoccitaneHandwash', 28000, '핸드워시', 100, 'seller');
 
 insert into Product(product_id, name, price, explanation, stock, seller)
-values (13, 'ipadMini', 700000, '아이패드미니', 300, 'Li-pot');
+values (13, 'ipadMini', 700000, '아이패드미니', 300, 'seller');
 
 insert into Product(product_id, name, price, explanation, stock, seller)
-values (14, 'ipadPro', 2000000, '아이패드프로', 400, 'Li-pot');
+values (14, 'ipadPro', 2000000, '아이패드프로', 400, 'seller');
 
